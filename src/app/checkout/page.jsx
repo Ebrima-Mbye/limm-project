3f852e9038bdc8cbed2da0932dd4b79601d4c828
Ebrima Mbye <ebrimam747@gmail.com>
2024-12-10 08:05:48 +0000

    Added styled the checkout page's checkout div... 10-12-24, 08:05, boys' new house.

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -5,7 +5,7 @@ import { getCart, deleteFromCart } from "../data/cart";
 import { useState } from "react";
 
 export default function Checkout() {
-  const [ cart, setCart] = useState(getCart());
+  const [cart, setCart] = useState(getCart());
 
   function reflectDelete(id) {
     deleteFromCart(id);
@@ -14,29 +14,36 @@ export default function Checkout() {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 p-4 ">
-      {cart.map((cartItem, index) => (
-        <div
-          key={index}
-          className="mb-7 max-w-[600px] border-gray-600 border-2 py-4 px-2"
-        >
-          <Image
-            src={cartItem.src}
-            alt={cartItem.productName}
-            className="w-full mx-auto max-w-[400px] mb-4 rounded-sm"
-          />
-          <div className="w-full flex justify-around mb-4">
-            <button className="bg-orange-500 px-4 py-2">Update</button>
-            <button className="bg-red-500 px-4 py-2" onClick={() => reflectDelete(cartItem.id)}>Delete</button>
+      <div>
+        {cart.map((cartItem, index) => (
+          <div
+            key={index}
+            className="mb-7 max-w-[600px] border-gray-600 border-2 py-4 px-2"
+          >
+            <Image
+              src={cartItem.src}
+              alt={cartItem.productName}
+              className="shadow-md w-full mx-auto max-w-[400px] mb-4 rounded-sm"
+            />
+            <div className="w-full flex justify-around mb-4">
+              <button className="bg-orange-500 px-4 py-2">Update</button>
+              <button
+                className="bg-red-500 px-4 py-2"
+                onClick={() => reflectDelete(cartItem.id)}
+              >
+                Delete
+              </button>
+            </div>
+            <div className="text-center">
+              <p>Name: {cartItem.productName}</p>
+              <p>Price: {cartItem.price}</p>
+            </div>
           </div>
-          <div className="text-center">
-            <p>Name: {cartItem.productName}</p>
-            <p>Price: {cartItem.price}</p>
-          </div>
-        </div>
-      ))}
-      <div className=" mt-5 border-2 border-white py-4 px-5">
-        <div className="mb text-center">
-            <p className="m-6  text-gray-600 font-extrabold text-5xl">Checkout</p>
+        ))}
+      </div>
+      <div className="sticky top-16 flex flex-col justify-around max-h-[300px] mt-5 border-2 border-gray-600 py-4 px-5">
+        <div className=" text-center">
+          <p className="m-6 text-gray-600 font-extrabold text-5xl">Checkout</p>
         </div>
         <div className="px-3">
           <p>Total: $450</p>
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <div>
         <a className='px-4 py-2 bg-gray-400 rounded-md text-gray-700 font-bold border-gray-400 border-2' href='/'>Logo</a>
       </div>
-      <div className="text-gray-500 font-bold text-2xl">Mbye's Online</div>
+      <div className="text-gray-500 font-bold text-2xl">Ebrima's Store</div>
       <div>
         <a className='px-4 py-2 bg-gray-400 rounded-md text-gray-700 font-bold border-gray-400 border-2' href='/checkout'>Cart</a>
       </div>
