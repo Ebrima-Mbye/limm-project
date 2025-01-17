0d5a38b382adf3fe54e95ef4e79e4a5f20602289
Ebrima Mbye <ebrimam747@gmail.com>
2025-01-17 15:56:59 +0000

    Fixed a comman, and done with the hero section:
    17-01-25, 15:56, Boys' new house.

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,48 +4,17 @@ import image2 from "../photos/woman-in-clothing-shop.png";
 import bookIcon from "../photos/bookkeeping-icon.svg";
 import inventoryIcon from "../photos/inventory-icon.svg";
 import { cookies } from "next/headers";
-import { pageHeading } from "../data/hero.js"
+import { pageHeading, bottomText, cards } from "../data/hero.js";
 
 export default async function HomeHero() {
   // Wait for cookies before accessing them
   const cookieStore = await cookies();
   const language = cookieStore.get("language")?.value || "en"; // Default to 'en'
-
-  const cards = {
-    en: [
-      {
-        title: "Bookkeeping",
-        text: "Ditch the spreadsheet and messy records. Limm automates bookkeeping, saving you time and ensuring accuracy.",
-      },
-      {
-        title: "Inventory Management",
-        text: "Limm&apos;s smart inventory management keeps you in control, minimizing stockouts and optimizing ordering.",
-      },
-    ],
-    fr: [
-      {
-        title: "Comptabilité",
-        text: "Abandonnez les feuilles de calcul et les registres désordonnés. Limm automatise la comptabilité, vous faisant gagner du temps et garantissant la précision.",
-      },
-      {
-        title: "Gestion des stocks",
-        text: "La gestion intelligente des stocks de Limm vous garde maître de la situation, minimisant les ruptures de stock et optimisant les commandes.",
-      },
-    ],
-    es: [
-      {
-        title: "Contabilidad",
-        text: "Deja atrás las hojas de cálculo y los registros desordenados. Limm automatiza la contabilidad, ahorrándote tiempo y garantizando precisión.",
-      },
-      {
-        title: "Gestión de inventarios",
-        text: "La gestión inteligente de inventarios de Limm te mantiene en control, minimizando las faltas de stock y optimizando los pedidos.",
-      },
-    ],
-  };
-
+  
   const heading = pageHeading(language);
   const cardsData = cards[language];
+  const bottomTextData = bottomText[language];
+
   return (
     // <section className="lg:h-[97vh] px-6 md:px-12 pt-20 pb-28 md:pb-4 mx-4 overflow-y-clip bg-gray-100 rounded-lg">
     <section className="mt-0 min-h-screen overflow-y-clip rounded-lg bg-gray-100 px-3 pb-28 pt-20 md:px-12 md:pb-8 lg:mx-4 lg:mt-4">
@@ -87,13 +56,9 @@ export default async function HomeHero() {
         </div>
         <div className="">
           <p className="mb-1 text-start text-[20px] font-medium leading-[24.5px] md:text-[22px]">
-            One Platform for Payments, Inventory, and Staff Management for SMWs
+            {bottomTextData.heading}
           </p>
-          <p className="text-slate-500">
-            Built for Africa, powered by technology. Limm is your one-stop shop
-            for <br /> streamlining operations, growing your business, and
-            thriving in the digital age.
-          </p>
+          <p className="text-slate-500">{bottomTextData.subText}</p>
         </div>
         <div className="mt-16 md:hidden lg:mt-0">
           <button className="ml-4 text-nowrap rounded-[50px] bg-slate-500 px-6 py-3 text-[20px] text-white lg:px-3 lg:py-1 lg:text-[12px]">
--- src/app/data/hero.js
+++ src/app/data/hero.js
@@ -4,10 +4,90 @@ export function pageHeading(language) {
       Empowering Businesses with Innovative <br /> Solutions.
     </>
   ) : language === "fr" ? (
-    <>Donner aux entreprises les moyens d&apos;agir avec des solutions Innovantes.</>
+    <>
+      Donner aux entreprises les moyens d&apos;agir avec des solutions
+      Innovantes.
+    </>
   ) : language === "es" ? (
     <>Empoderando a las empresas con soluciones Innovadoras.</>
   ) : (
     ""
   );
 }
+
+export const cards = {
+  en: [
+    {
+      title: "Bookkeeping",
+      text: "Ditch the spreadsheet and messy records. Limm automates bookkeeping, saving you time and ensuring accuracy.",
+    },
+    {
+      title: "Inventory Management",
+      text: (
+        <>
+          Limm&apos;s smart inventory management keeps you in control,
+          minimizing stockouts and optimizing ordering."
+        </>
+      ),
+    },
+  ],
+  fr: [
+    {
+      title: "Comptabilité",
+      text: "Abandonnez les feuilles de calcul et les registres désordonnés. Limm automatise la comptabilité, vous faisant gagner du temps et garantissant la précision.",
+    },
+    {
+      title: "Gestion des stocks",
+      text: "La gestion intelligente des stocks de Limm vous garde maître de la situation, minimisant les ruptures de stock et optimisant les commandes.",
+    },
+  ],
+  es: [
+    {
+      title: "Contabilidad",
+      text: "Deja atrás las hojas de cálculo y los registros desordenados. Limm automatiza la contabilidad, ahorrándote tiempo y garantizando precisión.",
+    },
+    {
+      title: "Gestión de inventarios",
+      text: "La gestión inteligente de inventarios de Limm te mantiene en control, minimizando las faltas de stock y optimizando los pedidos.",
+    },
+  ],
+};
+
+export const bottomText = {
+  en: {
+    heading:
+      "One Platform for Payments, Inventory, and Staff Management for SMWs",
+    subText: (
+      <>
+        Built for Africa, powered by technology. Limm is your one-stop shop for{" "}
+        <br />
+        streamlining operations, growing your business, and thriving in the
+        digital age.
+      </>
+    ),
+  },
+  fr: {
+    heading:
+      "Une plateforme pour les paiements, la gestion des stocks et du personnel pour les PME",
+    subText: (
+      <>
+        Conçu pour l'Afrique, alimenté par la technologie. Limm est votre
+        solution unique pour <br />
+        rationaliser les opérations, développer votre entreprise et prospérer à
+        l'ère numérique.
+      </>
+    ),
+  },
+  es: {
+    heading:
+      "Una plataforma para pagos, inventario y gestión de personal para PYMEs",
+    subText: (
+      <>
+        Diseñado para África, impulsado por la tecnología. Limm es tu solución
+        integral para <br />
+        optimizar operaciones, hacer crecer tu negocio y prosperar en la era
+        digital.
+      </>
+    ),
+  },
+};
