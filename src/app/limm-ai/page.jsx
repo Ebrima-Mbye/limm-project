f35e44bb5b04b42e765fe288955d65bb8d1e8e07
Ebrima Mbye <ebrimam747@gmail.com>
2025-02-27 22:33:26 +0000

    Improved the home icon in the ai page: 27-02-25, 22:33, ITG.

--- src/app/limm-ai/page.jsx
+++ src/app/limm-ai/page.jsx
@@ -52,12 +52,20 @@ export default function Home() {
     },
   ];
 
-  const [messages, setMessages] = useState(initialContext);
+  const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
   const [aiGeneratingRespponse, setAiGeneratingRespponse] = useState(false);
 
   useEffect(() => {
+    setMessages(initialContext);
     fetchGreeting();
+
+    return () => {
+      // Clean up on unmount - destroy this page
+      setMessages([]);
+      setInput("");
+      setAiGeneratingRespponse(false);
+    };
   }, []);
 
   const fetchGreeting = async () => {
@@ -141,8 +149,13 @@ export default function Home() {
         onSubmit={handleSubmit}
         className="flex h-full w-full items-center gap-2"
       >
-        <Link title="Go to Home" href="/">
-          <HomeIcon />
+        <Link
+          title="Go to Home"
+          className="fixed bottom-4 left-4 z-[100] flex rounded-full border shadow-md"
+          href="/"
+          onClick={() => setMessages([])}
+        >
+          <HomeIcon className="h-12 w-12 cursor-pointer p-3" />
         </Link>
         <input
           type="text"
@@ -188,9 +201,13 @@ export default function Home() {
 
   return (
     <FullPage>
-      <main className="flex min-h-screen flex-col items-center p-12 font-[family-name:var(--font-geist-sans)] dark:text-black">
+      <main className="flex min-h-screen flex-col items-center p-4 font-[family-name:var(--font-geist-sans)] dark:text-black md:p-12">
         {RenderForm()}
-        {aiGeneratingRespponse && <p className="text-sm text-gray-500">Limm AI is generating a response...</p>}
+        {aiGeneratingRespponse && (
+          <p className="text-sm text-gray-500">
+            Limm AI is generating a response...
+          </p>
+        )}
         {RenderMessages()}
       </main>
     </FullPage>
